180901e614654c585ed7ca56d9218b7fd1735010
Quentin Comte-Gaz <quentin@comte-gaz.com>
2016-07-03 02:14:25 +0200

    lib: Add main RFID Card Reader library
    Add the most important part of this project (the lib).
    A keywords.txt file has been added to improve lib display
    in arduino interface.

---
+++ RFIDCardReader/RFIDCardReader.cpp
@@ -0,0 +1,142 @@
+/*
+ * \brief Get RFID Card/Tag ID with RFID Card Reader (implementation)
+ *
+ * \author Quentin Comte-Gaz <quentin@comte-gaz.com>
+ * \date 2 July 2016
+ * \license MIT License (contact me if too restrictive)
+ * \copyright Copyright (c) 2016 Quentin Comte-Gaz
+ * \version 1.0
+ */
+
+#include "HardwareSerial.h"
+#include "RFIDCardReader.h"
+
+//--------------------- Singleton (& private Constructor) ---------------------
+RFIDCardReader* RFIDCardReader::_instance = NULL;
+
+RFIDCardReader* RFIDCardReader::getInstance()
+{
+  if (!_instance) {
+    _instance = new RFIDCardReader();
+  }
+  return _instance;
+}
+
+RFIDCardReader::RFIDCardReader() :
+  _attachedFunction (NULL),
+  _serial (NULL),
+  _pending_rx_card (""),
+  _last_rx_card (""),
+  _ms_since_last_card_read (0),
+  _lock (false)
+{
+  // Associate correct serial port to the class
+  #if defined(HAVE_HWSERIAL1) && defined(RFIDCardReaderPort1)
+    _serial = &Serial1;
+  #elif defined(HAVE_HWSERIAL0) && defined(RFIDCardReaderPort1)
+    _serial = &Serial;
+  #elif defined(HAVE_HWSERIAL2) && defined(RFIDCardReaderPort2)
+    _serial = &Serial2;
+  #elif defined(HAVE_HWSERIAL3) && defined(RFIDCardReaderPort3)
+    _serial = &Serial3;
+  #else
+    ERROR, PLEASE DEFINE AT LEAST ONE CORRECT RFIDCardReaderPort IN RFIDCardReader.h
+  #endif
+}
+
+//----------------------------- Public functions ------------------------------
+void RFIDCardReader::init(int baudrate)
+{
+  _serial->begin(baudrate);
+}
+
+bool RFIDCardReader::getLastCard(String & data, unsigned long &ms_since_reception, bool reset)
+{
+  data = _last_rx_card;
+  unsigned long current_time = millis();
+  ms_since_reception = current_time - _ms_since_last_card_read;
+
+  // Note: This assertion may be wrong once every 50day because of millis() implementation
+  // https://www.arduino.cc/en/Reference/Millis
+  bool last_card_valid = (current_time >= _ms_since_last_card_read) &&
+                         (_ms_since_last_card_read != 0);
+  if (!last_card_valid) {
+    data = "";
+    ms_since_reception = 0;
+  }
+
+  if (last_card_valid && reset) {
+    resetLastCard();
+  }
+
+  return last_card_valid;
+}
+
+void RFIDCardReader::resetLastCard()
+{
+  while (_lock) {
+    delay(1);
+  }
+  _lock = true;
+  _last_rx_card = "";
+  _ms_since_last_card_read = 0;
+  _lock = false;
+}
+
+void RFIDCardReader::attachFunctionToCardDetectionEvent(void (*pfunction)())
+{
+  _attachedFunction = pfunction;
+}
+
+//---------------------------- Internal functions -----------------------------
+
+void RFIDCardReader::_oneCharReceived(char in_char)
+{
+  //Serial.print(in_char);
+  if ((in_char >= 48 && in_char <= 57) ||
+      (in_char >= 65 && in_char <= 90) ||
+      (in_char >= 97 && in_char <= 122)) {
+    _pending_rx_card.concat(in_char);
+  } else {
+    if (_pending_rx_card.length() > 0 && !_lock) {
+      _lock = true; // To have no issue between reset and reception
+      _last_rx_card = _pending_rx_card;
+      _ms_since_last_card_read = millis();
+      _lock = false;
+      if (_attachedFunction != NULL) {
+        _attachedFunction();
+      }
+    }
+    _pending_rx_card = "";
+  }
+}
+
+//////////////////// Serial Events /////////////////////
+
+#if defined(HAVE_HWSERIAL1) && defined(RFIDCardReaderPort1) // Arduino MEGA Only: UART 1
+void serialEvent1() {
+  while (Serial1.available()) {
+    RFIDCardReader::getInstance()->_oneCharReceived((char)Serial1.read());
+  }
+}
+#elif defined(HAVE_HWSERIAL0) && defined(RFIDCardReaderPort1) // All Arduino: UART 0
+void serialEvent() {
+  while (Serial.available()) {
+    RFIDCardReader::getInstance()->_oneCharReceived((char)Serial.read());
+  }
+}
+#elif defined(HAVE_HWSERIAL2) && defined(RFIDCardReaderPort2) // Arduino MEGA Only: UART 2
+void serialEvent2() {
+  while (Serial2.available()) {
+    RFIDCardReader::getInstance()->_oneCharReceived((char)Serial2.read());
+  }
+}
+#elif defined(HAVE_HWSERIAL3) && defined(RFIDCardReaderPort3) // Arduino MEGA Only: UART 3
+void serialEvent3() {
+  while (Serial3.available()) {
+    RFIDCardReader::getInstance()->_oneCharReceived((char)Serial3.read());
+  }
+}
+#else
+  ERROR, PLEASE DEFINE AT LEAST ONE CORRECT RFIDCardReaderPort IN RFIDCardReader.h
+#endif
---
+++ RFIDCardReader/RFIDCardReader.h
@@ -0,0 +1,93 @@
+/*
+ * \brief Get RFID Card/Tag ID with RFID Card Reader
+ *
+ * \author Quentin Comte-Gaz <quentin@comte-gaz.com>
+ * \date 2 July 2016
+ * \license MIT License (contact me if too restrictive)
+ * \copyright Copyright (c) 2016 Quentin Comte-Gaz
+ * \version 1.0
+ */
+
+#ifndef RFIDCardReader_h
+#define RFIDCardReader_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+// Only one port must be uncommented
+#define RFIDCardReaderPort1 // Base UART port available on all Arduino devices
+//#define RFIDCardReaderPort2 // Usable only with Arduino MEGA
+//#define RFIDCardReaderPort3 // Usable only with Arduino MEGA
+
+class HardwareSerial;
+
+class RFIDCardReader
+{
+  public:
+    /*!
+     * \brief getInstance Get a pointer to RFIDCardReader class
+     *
+     * \return (RFIDCardReader*) Pointer to RFIDCardReader class
+     */
+    static RFIDCardReader* getInstance();
+
+    /*!
+     * \brief init Initialize class
+     *
+     * \param baudrate (int) Baudrate of RFID Card reader (most of the time: 9600)
+     */
+    void init(int baudrate = 9600);
+
+    /*!
+     * \brief getLastCard Get the last Card ID found by the RFID Card Reader
+     *
+     * \param data (String)[out] Baudrate of RFID Card reader (most of the time: 9600)
+     * \param ms_since_reception (unsigned long)[out] Time passed since the card was found
+     * \param reset (bool)[in] Clean the card ID from buffer (else, the card will be stored until a card is detected).
+     *
+     * \return One card was received (--> values \p data and \p ms_since_reception are correct)
+     */
+    bool getLastCard(String &data, unsigned long &ms_since_reception, bool reset = true);
+
+    /*!
+     * \brief resetLastCard Clean the card ID from buffer
+     *
+     * This will force \p getLastCard to return false until a new card is detected.
+     */
+    void resetLastCard();
+
+    /*!
+     * \brief attachFunctionToCardDetectionEvent Attach a specific user function to "Card detected" event
+     *
+     * \param (void function()) Function that will be launched every time a card is detected
+     */
+    void attachFunctionToCardDetectionEvent(void (*pfunction)());
+
+
+
+
+  //---------------------------------------------------------------------------
+  //-------------------------- Implementation details -------------------------
+  //---------------------------------------------------------------------------
+  public:
+    /*! !!Internal implementation!! (get one char by one char from RFID reader).
+        Must be public because of HardwareSerial Arduino library implementation. */
+    void _oneCharReceived(char in_char);
+
+  private:
+    RFIDCardReader(); /*!< Private constructor, please use RFIDCardReader::getInstance() to initialize the class */
+
+  private:
+    static RFIDCardReader* _instance; /*!< Singleton instance */
+    void (*_attachedFunction)(); /*!< Attached function to launch every time a new card is fully received */
+    HardwareSerial *_serial; /*!< Pointer to serial port used to communicate with the RFID card reader */
+    String _pending_rx_card; /*!< Card ID which is in process to be retrieved */
+    String _last_rx_card;  /*!< Last full received card ID */
+    unsigned long _ms_since_last_card_read; /*!< Time since _last_rx_card has been received */
+    bool _lock; /*!< Lock _last_rx_card and _ms_since_last_card_read (write vs reset) */
+};
+
+#endif //RDIFCardReader_h
